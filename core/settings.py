cdd9761d80c8ab2ecc17d2731ef91c709ad003dc
Ayush Verma <106526797+CrimsonREwind@users.noreply.github.com>
2026-02-27 00:25:17 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = 'static/'
-STATICFILES_DIRS = []
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
